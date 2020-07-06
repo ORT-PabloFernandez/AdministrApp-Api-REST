c7c0302d6a9fd9144cac9ba7027e1f14bc115408
Seba <sebastianpanetta@gmail.com>
2020-07-06 19:10:26 -0300

    Agregado update y delete para usuarios

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -21,6 +21,7 @@ function signUp(req, res) {
         if (err) return res.status(500).send({ message: `Error al crear el usuario: ${err}` });
         console.log("Llamando servicio de creacion de token...");
         return res.status(200).send({
+            id: usuario._id,
             nombre: req.body.nombre,
             apellido: req.body.apellido,
             email: req.body.email,
@@ -46,6 +47,7 @@ function signIn(req, res) {
             console.log("Verificacion de password OK");
             req.usuario = usuario;
             return res.status(200).send({
+                id: usuario._id,
                 nombre: usuario.nombre,
                 apellido: usuario.apellido,
                 email: usuario.email,
@@ -59,9 +61,57 @@ function signIn(req, res) {
     }).select('_id nombre apellido email telefono nombreUsuario cuit tipo +password');
 }
 
+function updateUsuario(req, res) {
+    let userId = req.params.userId;
+    console.log("getUsuario Id: " + userId);
+    Usuario.findById(userId, (err, usuario) => {
+        if (err) return res.status(500).send({ message: `Error al realizar la peticion: ${err}` });
+        if (!usuario) return res.status(404).send({ message: `El usuario no existe` });
+        console.log("Usuario: " + JSON.stringify(usuario));
+        usuario.nombre = req.body.nombre;
+        usuario.apellido = req.body.apellido;
+        usuario.email = req.body.email;
+        usuario.telefono = req.body.telefono;
+        usuario.nombreUsuario = req.body.nombreUsuario;
+        usuario.password = req.body.password;
+        usuario.cuit = req.body.cuit;
+        usuario.tipo = req.body.tipo;
+        console.log("Modificando usuario: " + usuario.email);
+        usuario.save((err) => {
+            if (err) return res.status(500).send({ message: `Error al modificar el usuario: ${err}` });
+            return res.status(200).send({
+                id: usuario._id,
+                nombre: req.body.nombre,
+                apellido: req.body.apellido,
+                email: req.body.email,
+                telefono: req.body.telefono,
+                nombreUsuario: req.body.nombreUsuario,
+                cuit: req.body.cuit,
+                tipo: req.body.tipo
+            });
+        });
+    });
+}
+
+function deleteUsuario(req, res) {
+    let userId = req.params.userId;
+    console.log("deleteUsuario Id: " + userId);
+    Usuario.findById(userId, (err, usuario) => {
+        if (err) return res.status(500).send({ message: `Error al borrar el usuario: ${err}` });
+        if (!usuario) return res.status(404).send({ message: `No existe el usuario` });
+        usuario.remove(err => {
+            if (err) return res.status(500).send({ message: `Error al borrar el usuario: ${err}` });
+            console.log("usuario remove OK");
+            res.status(200).send({ message: 'El usuario ha sido eliminado' });
+        });
+    });
+}
+
 module.exports = {
     signUp,
-    signIn
+    signIn,
+    updateUsuario,
+    deleteUsuario
 }
 
 
